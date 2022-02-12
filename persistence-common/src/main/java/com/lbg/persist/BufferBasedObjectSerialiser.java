413fde912e912e1923e9f53d82899080c9e6a953
Juan Cabrito <git@photiczone.com>
2022-02-12 14:33:09 +0000

    added default constructor for the BufferBasedObjectSerialiser because my persist-phoenix project used it

--- persistence-common/src/main/java/com/lbg/persist/BufferBasedObjectSerialiser.java
+++ persistence-common/src/main/java/com/lbg/persist/BufferBasedObjectSerialiser.java
@@ -14,41 +14,40 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * an object serialiser that 
+ * an object serialiser that
+ * 
  * @author C006011
  */
-public class BufferBasedObjectSerialiser implements ObjectSerialiser
-{
+public class BufferBasedObjectSerialiser implements ObjectSerialiser {
 	private static final Logger log = LoggerFactory.getLogger(BufferBasedObjectSerialiser.class);
 
 	private final int bufferSize;
-	
-	public BufferBasedObjectSerialiser(int bufferSize)
-	{
+
+	/**
+	 * added for phoenix 2022
+	 */
+	public BufferBasedObjectSerialiser() {
+		this.bufferSize = 4096;
+	}
+
+	public BufferBasedObjectSerialiser(int bufferSize) {
 		this.bufferSize = bufferSize;
 	}
 
 	@Override
-	public byte[] write(Serializable object) throws PhysicalResourceException
-	{
+	public byte[] write(Serializable object) throws PhysicalResourceException {
 		final ByteArrayOutputStream baos = new ByteArrayOutputStream(bufferSize);
 		final ObjectOutputStream oos;
 
-		try
-		{
+		try {
 			oos = new ObjectOutputStream(baos);
-		}
-		catch (IOException ioe)
-		{
+		} catch (IOException ioe) {
 			throw new PhysicalResourceException("problem creating object output stream", ioe);
 		}
 
-		try
-		{
+		try {
 			oos.writeObject(object);
-		}
-		catch (IOException ioe)
-		{
+		} catch (IOException ioe) {
 			throw new PhysicalResourceException("problem writing object to output stream", ioe);
 		}
 
@@ -58,46 +57,33 @@ public class BufferBasedObjectSerialiser implements ObjectSerialiser
 	}
 
 	@Override
-	public <T> T read(byte[] data) throws PhysicalResourceException
-	{
+	public <T> T read(byte[] data) throws PhysicalResourceException {
 		return read(data, 0, data.length);
 	}
 
-	@Override @SuppressWarnings("unchecked")
-	public <T> T read(byte[] data, int offset, int length) throws PhysicalResourceException
-	{
+	@Override
+	@SuppressWarnings("unchecked")
+	public <T> T read(byte[] data, int offset, int length) throws PhysicalResourceException {
 		final ByteArrayInputStream bais = new ByteArrayInputStream(data, offset, length);
 		final ObjectInputStream ois;
 
-		try
-		{
+		try {
 			ois = new ObjectInputStream(bais);
-		}
-		catch (StreamCorruptedException sce)
-		{
+		} catch (StreamCorruptedException sce) {
 			throw new PhysicalResourceException("stream corruption detected on object input stream construction", sce);
-		}
-		catch (IOException ioe)
-		{
+		} catch (IOException ioe) {
 			throw new PhysicalResourceException("I/O exception occurred on object input stream construction", ioe);
 		}
 
 		T object = null;
 
-		try
-		{
-			object = (T)ois.readObject();
-		}
-		catch (ClassNotFoundException cnfe)
-		{
+		try {
+			object = (T) ois.readObject();
+		} catch (ClassNotFoundException cnfe) {
 			throw new PhysicalResourceException("class not found during object read", cnfe);
-		}
-		catch (OptionalDataException ode)
-		{
+		} catch (OptionalDataException ode) {
 			throw new PhysicalResourceException("optional data problem during object read", ode);
-		}
-		catch (IOException ioe)
-		{
+		} catch (IOException ioe) {
 			throw new PhysicalResourceException("I/O exception during object read", ioe);
 		}
 
